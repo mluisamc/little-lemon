c6152fee4f80a40ddba751b48048225694b02ebd
María Luisa Martínez <mluisamartinezc@gmail.com>
2023-03-09 09:39:10 +0100

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import Header from './Header.js';
+import Nav from './Nav.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
-  );
+    <>
+      <head>
+        <meta name="description" content="Little Lemon restaurant with wide variety of dishes and affordable prices"/>
+        <meta name="og:title" content="Little Lemon restaurant"/>
+        <meta name="og:description" content="Little Lemon restaurant with wide variety of dishes and affordable prices"/>
+        <meta name="og:image" content=""/>
+      </head>
+      <body>
+        <Header/>
+        <Nav/>
+        <Main/>
+        <Footer/>
+      </body>
+    </>
+  )
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <img src="icons_assets/logo.png" alt="Little Lemon logo"></img>
+        </header>
+    )
+}
+
+export default Header;
